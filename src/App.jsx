ebe352de640e9b076fd312583dff0d0721289ae9
anvDT <devabvsng@gmail.com>
2025-10-02 02:08:28 +0530

    AccPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 ﻿import "./App.css";
 
-import AccordionGroup from "./Components/Controlled Accordion/AccordionGroup";
 
 function App() {
   return (
     <>
-      <AccordionGroup />
+
     </>
   );
 }
---
+++ src/Pages/AccordionPage.jsx
@@ -0,0 +1,13 @@
+import Accordion from "../Components/Accordion";
+export default function AccordionPage() {
+  return (
+    <>
+      <Accordion title={"example"}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt aut nisi
+        cum beatae odit. Laboriosam suscipit dicta ipsam itaque ut asperiores
+        laudantium expedita, hic voluptas necessitatibus vero deleniti aliquid
+        labore?
+      </Accordion>
+    </>
+  );
+}
